72118c83ac6e397d417681f60a04a774566f5cd8
Bohdan Shaban <bohdanshaban97@gmail.com>
2021-08-02 14:23:10 +0200

    Proj Init

---
+++ src/components/app/app.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+
+
+import './app.sass';
+
+
+export default class App extends Component {
+
+
+
+    render() {
+
+        return (
+            <div>
+                HELLO !!! !!!
+            </div>
+        )
+    }
+}
